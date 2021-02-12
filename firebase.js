0d6284d30755f7155f32c46688c24b5da6cf02d8
vistritpandey <vistritpandey@gmail.com>
2021-02-12 15:18:17 +0530

    Firebase added

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDiER7W5Ei6gZXoPosso_vFzzbigjBWj6g",
+  authDomain: "vistrit-signal.firebaseapp.com",
+  projectId: "vistrit-signal",
+  storageBucket: "vistrit-signal.appspot.com",
+  messagingSenderId: "539954893808",
+  appId: "1:539954893808:web:7c14f0888eada54ec64335",
+  measurementId: "G-3KQ3J97PVF",
+};
